0861ae3fc7d2336b6b84434568b2310fcf6a97bd
Francisco de Babo Martins <x2francisco@gmail.com>
2018-05-26 22:22:09 +0100

    Added an useful and insightful comment

--- main.cpp
+++ main.cpp
@@ -35,6 +35,12 @@ int main() {
     // Create a thread
     {
         auto thread = std::thread(&thread_function);
+        
+        /**
+         * The function returns when the thread execution has completed.
+         * This synchronizes the moment this function returns with the completion of all the operations in the thread:
+         * This blocks the execution of the thread that calls this function until the function called on construction returns
+         */
         thread.join();
     }
 
